3c025e7ded2061537af34276d4ee6fdf987047c3
kroniz-utab <adehf12@gmail.com>
2021-11-20 00:09:13 +0700

    adding CI and Crashlytics integration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # a199-flutter-expert-project
+
+[![Codemagic build status](https://api.codemagic.io/apps/61976348e2c8da10e24cf4b6/61976348e2c8da10e24cf4b5/status_badge.svg)](https://codemagic.io/apps/61976348e2c8da10e24cf4b6/61976348e2c8da10e24cf4b5/latest_build)
--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   di.init();
   runApp(MyApp());
 }
