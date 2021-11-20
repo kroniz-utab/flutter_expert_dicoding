3a5724a7359c3a44c2da97700089e7191d8e396b
kroniz-utab <adehf12@gmail.com>
2021-11-20 10:49:59 +0700

    fixing search page

--- search/lib/presentation/pages/search_page.dart
+++ search/lib/presentation/pages/search_page.dart
@@ -7,7 +7,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:search/search.dart';
 
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class SearchPage extends StatelessWidget {
   final bool isMovieSearch;
